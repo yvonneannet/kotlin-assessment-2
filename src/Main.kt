7f35c35d7be6da0ecf9107ea49656a201c6d8f7a
Student <yvonneoyellaa@gmail.com>
2024-03-25 08:18:05 +0300

    attempted

---
+++ src/Main.kt
@@ -0,0 +1,41 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    nums(20,43,12,6)
+
+
+
+}
+//2.
+fun nums (b1:Int,b2:Int,b3:Int,b4:Int): Int{
+    val nums = arrayOf(b1,b2,b3,b4)
+    val added= (nums.sum())
+    return (added)
+
+    val count= (nums.count())
+    return count
+
+    val averageOfNums= (nums.average())
+    return (averageOfNums)
+}
+
+//3.
+fun volumeOfSphere(){
+
+
+}
+
+//1.
+fun extract (){
+    val words= ("Barnie bakes brown bagels and buns")
+    var newWords: String= for ('b' in words ){
+        (words.extract 'b')
+        return(newWords)
+    }
+}
+
+//.4
+fun
+
+
+

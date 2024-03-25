c571756976475b8f471e2e4e13e46737965f8c97
Student <yvonneoyellaa@gmail.com>
2024-03-25 08:59:40 +0300

    made changes

--- src/Main.kt
+++ src/Main.kt
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     numbs(20,43,12,6)
-
+volumeOfSphere(numbb = 31)
 
 
 }
@@ -18,18 +18,31 @@ fun numbs(b1:Int,b2:Int,b3:Int,b4:Int){
 
 
 ////3.
-fun volumeOfSphere(){
-}
+fun volumeOfSphere(numbb:Int){
+    var volume= 4/3*3.14159*(numbb*3)
+    println(volume)}
 
-//1.
-fun extract (){
-   val words= ("Barnie bakes brown bagels and buns")
-    var newWords: String= for ('b' in words ){
-       (words.extract 'b')
-       println(newWords)
-   }
-}
 
-////.4
-fun
-
+//}
+//
+////1.
+//fun extract (){
+//   val words= ("Barnie bakes brown bagels and buns")
+//    var newWords: String= for ('b' in words ){
+//       (words.extract 'b')
+//       println(newWords)
+//   }
+//}
+//words.forEach{x-> println(x) } for(x in words){ println(x)} for(y in nums){
+//    println(y*2) println(y(2)) } }
+//
+//////.4
+//fun isPalindrome(word:String) {
+//    if (text)
+//        println ("true")}
+//    else {
+//    println("false")
+//
+//
+//}
+//

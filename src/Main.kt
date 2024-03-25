189ba0b5082fd4d41d8a8cdba90363ab8b1ed65f
Student <yvonneoyellaa@gmail.com>
2024-03-25 08:39:10 +0300

    made changes

--- src/Main.kt
+++ src/Main.kt
@@ -1,41 +1,35 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    nums(20,43,12,6)
+    numbs(20,43,12,6)
 
 
 
 }
 //2.
-fun nums (b1:Int,b2:Int,b3:Int,b4:Int): Int{
-    val nums = arrayOf(b1,b2,b3,b4)
-    val added= (nums.sum())
-    return (added)
-
-    val count= (nums.count())
-    return count
-
-    val averageOfNums= (nums.average())
-    return (averageOfNums)
+fun numbs(b1:Int,b2:Int,b3:Int,b4:Int){
+    var numbs = arrayOf(b1,b2,b3,b4)
+    println(numbs.sum())
+    println (numbs.count())
+        println(numbs.average())
 }
 
-//3.
+
+
+
+////3.
 fun volumeOfSphere(){
-
-
 }
 
 //1.
 fun extract (){
-    val words= ("Barnie bakes brown bagels and buns")
+   val words= ("Barnie bakes brown bagels and buns")
     var newWords: String= for ('b' in words ){
-        (words.extract 'b')
-        return(newWords)
-    }
+       (words.extract 'b')
+       println(newWords)
+   }
 }
 
-//.4
+////.4
 fun
-
-
 
